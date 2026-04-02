163af695eab8a8d77d2ac7703f88336232ee48f3
Yao <e1349011@u.nus.edu>
2026-04-02 11:34:56 +0800

    delete other pages

--- components/app-router.tsx
+++ components/app-router.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { AuthProvider } from "@/contexts/auth-context"
 import { ChatProvider } from "@/contexts/chat-context"
-import { ProtectedLayout } from "./protected-layout"
-import { LoginPage } from "./login-page"
 import { ClassifyPage } from "./classify-page"
-import { InsightsPage } from "./insights-page"
-import { BudgetPage } from "./budget-page"
+import { LoginPage } from "./login-page"
+import { ProtectedLayout } from "./protected-layout"
 
 export function AppRouter() {
   return (
@@ -15,20 +13,13 @@ export function AppRouter() {
       <AuthProvider>
         <ChatProvider>
           <Routes>
-            {/* Public routes */}
             <Route path="/login" element={<LoginPage />} />
 
-            {/* Protected routes with shared layout */}
             <Route element={<ProtectedLayout />}>
               <Route path="/classify" element={<ClassifyPage />} />
-              <Route path="/insights" element={<InsightsPage />} />
-              <Route path="/budget" element={<BudgetPage />} />
             </Route>
 
-            {/* Redirect root to classify */}
             <Route path="/" element={<Navigate to="/classify" replace />} />
-            
-            {/* Catch all - redirect to classify */}
             <Route path="*" element={<Navigate to="/classify" replace />} />
           </Routes>
         </ChatProvider>
